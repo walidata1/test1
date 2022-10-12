a659034e4ee0395abc23f501427a19bf5c807835
Walid Ahmeddin <112585598+walidata1@users.noreply.github.com>
2022-10-13 00:21:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,66 +1,124 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const logoutForm = document.querySelector("#logout");
+const username = document.getElementById('username');
+const password = document.getElementById('password');
 
-document.querySelector("#linkLogout").addEventListener("click", e => {
-    e.preventDefault();
-    loginForm.classList.add("form--hidden");
-    logoutForm.classList.remove("form--hidden");
+const logInBtn = document.getElementById('LogInBtn');
+const logInLink = document.getElementById('logInLink');
+
+const loginPage = document.querySelector('.loginP');
+const welPage = document.querySelector('.welPage');
+
+// Användare
+const user = [
+  {
+    username: 'fredrik',
+    password: '12345',
+  },
+];
+
+
+if (!localStorage.getItem('user')) {
+  localStorage.setItem('user', JSON.stringify(user));
+}
+// kontrolera om inloggningen är lyckad
+const correctLogIn = () => {
+  const loggedIn = JSON.parse(localStorage.getItem('loggedIn'));
+  const loggedInNow  = JSON.parse(localStorage.getItem('userLoggedInNow')
+  );
+
+  if (loggedIn && loggedInNow) {
+    signInSucessful();
+  }
+};
+correctLogIn();
+
+// Om inloggning misslyckades visar ERROR meddelande
+function showError(message) {
+  const errorMsgLogin = document.getElementById('errorMsgLogin');
+  errorMsgLogin.innerHTML = message;
+}
+
+// Kontrolera om användaren finns i listan eller inte
+const userInLista = () => {
+  const getUserInLista = JSON.parse(localStorage.getItem('user'));
+  const userFound = getUserInLista.find(user => user.username === username.value && user.password === password.value);
+
+  if (userFound) {
+    localStorage.setItem('loggedIn', JSON.stringify(true));
+    const isUserLoggedIn = JSON.parse(localStorage.getItem('loggedIn'));
+    localStorage.setItem('userLoggedInNow', JSON.stringify(username.value));
+    const currentLoggedInUser = JSON.parse(
+      localStorage.getItem('userLoggedInNow')
+    );
+
+    if (isUserLoggedIn && currentLoggedInUser) {
+      signInSucessful();
+    }
+  } else {
+    signInFail();
+  }
+};
+
+function signInSucessful() {
+  showWelPage();
+}
+
+const signInFail = () => {
+  if (username.value === '' || password.value === '') {
+    showError('Fyll i dina inloggningsuppgifter');
+  } else {
+    showError('Du har angett fel uppgifter. Testa igen..');
+  }
+  clearFields();
+};
+
+function LoggaUt() {
+  loginPage.style.display = 'flex';
+  welPage.style.display = 'none';
+  document.getElementById('userLoggedIn').innerHTML = '';
+  clearFields();
+  localStorage.setItem('loggedIn', JSON.stringify(false));
+  localStorage.removeItem('userLoggedInNow');
+}
+
+
+// Om användaren finns i listan anropa userInLista function
+logInBtn.addEventListener('click', event => {
+  event.preventDefault();
+  userInLista();
 });
 
-document.querySelector("#linkLogin").addEventListener("click", e => {
-    e.preventDefault();
 
-    loginForm.classList.remove("form--hidden");
-    logoutForm.classList.add("form--hidden");
-});
+// Sidan visar dynamiskt rätt innehåll 
+function showWelPage() {
+  loginPage.style.display = 'none';
+  welPage.style.display = 'flex';
+  createLogOutBtn();
+  greeting();
+}
 
-loginForm.addEventListener("submit", e => {
-    e.preventDefault();
+// Säg välkommen till användaren
+function greeting() {
+  document.getElementById('userLoggedIn').innerHTML = "Välkommen!";
+}
 
-    // Perform your AJAX/Fetch login
+// Skapa logga ut knapp
+function createLogOutBtn() {
+  const welPageContainer = document.createElement('div');
+  
+  const signOutBtn = document.createElement('button');
+  signOutBtn.innerText = 'Logga ut';
+  signOutBtn.onclick = () => {
+    welPageContainer.remove();
+    LoggaUt();
+  };
+  signOutBtn.classList.add('signOutBtn');
+  welPageContainer.append(signOutBtn);
+  welPage.appendChild(welPageContainer);
+}
 
-    setFormMessage(loginForm, "error", "Invalid username/password combination");
-});
-
-document.querySelectorAll(".form__input").forEach(inputElement => {
-    inputElement.addEventListener("blur", e => {
-        if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-            setInputError(inputElement, "Username must be at least 10 characters in length");
-        }
-    });
-
-    inputElement.addEventListener("input", e => {
-        clearInputError(inputElement);
-    });
-});
-});
-
-
-
-function test() {
-
-    //Retriving data
-    var uid = document.getElementById("username").value;
-    var pw = document.getElementById("password").value;
-
-    //Storing data
-    var user = localStorage.setItem("uid", uid);
-    var pass = localStorage.setItem("pw", pw);
-
-
-    //Retriving stored data and using it 
-    var user = localStorage.getItem("uid", uid);
-    var pass = localStorage.getItem("pw", pw);
-
-
-    var a, b;
-    a = "fredrik";
-    b= 12345;
-
-    if (a == user && b == pass) {
-
-        alert(uid + pw)
-    
-
-    }}
+// Tömma formuläret vid fel inloggning
+const clearFields = () => {
+  const inputs = document.querySelectorAll('#username, #password');
+  inputs.forEach(input => {input.value = '';
+  });
+};
